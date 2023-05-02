9cbde2bef3b850f6e9652cf34e997f9a17bfa819
Nathan Rajlich <n@n8.io>
2023-05-01 20:45:28 -0700

    Fix edge output files

--- packages/vercel-remix/rollup.config.js
+++ packages/vercel-remix/rollup.config.js
@@ -1,11 +1,18 @@
+const { dirname } = require('path');
+
 const { getAdapterConfig } = require("../../rollup.utils");
 
 /** @returns {import("rollup").RollupOptions[]} */
 module.exports = function rollup() {
+  // Since `edge/index.ts` references files above the "edge" dir,
+  // rollup ends up outputting files into `dist/edge/edge`, so
+  // set the output dir to `dist` instead of `dist/edge`.
+  let edgeConfig = getAdapterConfig("vercel-remix", "edge/index.ts");
+  edgeConfig.output.dir = dirname(edgeConfig.output.dir);
   return [
     getAdapterConfig("vercel-remix"),
     getAdapterConfig("vercel-remix", "server.ts"),
-    getAdapterConfig("vercel-remix", "edge/index.ts"),
+    edgeConfig,
     getAdapterConfig("vercel-remix", "edge/server.ts"),
   ];
 };
