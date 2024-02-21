ffcec4cd2457de5c870cb03a1538ade26b72d7b5
Sean Massa <endangeredmassa@gmail.com>
2024-02-21 17:18:09 -0600

    refactor remix vite config

--- packages/vercel-remix/vite.ts
+++ packages/vercel-remix/vite.ts
@@ -2,7 +2,7 @@ import { Project } from "ts-morph";
 import { join, basename, extname } from "path";
 import { mkdirSync, writeFileSync, cpSync, rmSync, readdirSync } from "fs";
 import { getConfig, type BaseFunctionConfig } from "@vercel/static-config";
-import type { Preset } from "@remix-run/dev/vite/plugin";
+import type { Preset, VitePluginConfig } from "@remix-run/dev/vite/plugin";
 import type { ConfigRoute } from "@remix-run/dev/config/routes";
 
 function hash(config: Record<string, unknown>): string {
@@ -39,6 +39,80 @@ export function vercelPreset(): Preset {
     return config;
   }
 
+  function createServerBundles(remixUserConfig: VitePluginConfig): VitePluginConfig['serverBundles'] {
+    return ({ branch }) => {
+      let config = getRouteConfig(branch);
+      if (!config.runtime) {
+        config.runtime = "nodejs";
+      }
+
+      // If there are any "edge" runtime routes, then a special
+      // `entry.server` needs to be used. So copy that file into
+      // the app directory, unless the project has defined their own
+      if (config.runtime === "edge" && !entryServerPath) {
+        let appDirectory = remixUserConfig.appDirectory ?? "app";
+        let entryServerFile = readdirSync(appDirectory).find(
+          (f) => basename(f, extname(f)) === 'entry.server'
+        );
+        if (entryServerFile) {
+          entryServerPath = join(appDirectory, entryServerFile);
+        } else {
+          entryServerPath = join(appDirectory, "entry.server.jsx");
+          addedEntryServer = true;
+          cpSync(
+            join(__dirname, "defaults/entry.server.jsx"),
+            entryServerPath
+          );
+        }
+      }
+
+      config = flattenAndSort(config);
+      let id = `${config.runtime}-${hash(config)}`;
+      if (!bundleConfigs.has(id)) {
+        bundleConfigs.set(id, config);
+      }
+      return id;
+    };
+  }
+
+  let buildEnd: VitePluginConfig['buildEnd'] = ({ buildManifest, remixConfig }) => {
+    if (addedEntryServer && entryServerPath) {
+      rmSync(entryServerPath);
+    }
+
+    if (buildManifest?.serverBundles && bundleConfigs.size) {
+      for (let bundle of Object.values(buildManifest.serverBundles)) {
+        let bundleWtihConfig = {
+          ...bundle,
+          config: bundleConfigs.get(bundle.id),
+        };
+        buildManifest.serverBundles[bundle.id] = bundleWtihConfig;
+      }
+    }
+
+    if (buildManifest?.routes && routeConfigs.size) {
+      for (let route of Object.values(buildManifest.routes)) {
+        let routeWtihConfig = {
+          ...route,
+          config: routeConfigs.get(route.id),
+        };
+        buildManifest.routes[route.id] = routeWtihConfig;
+      }
+    }
+    
+    let json = JSON.stringify(
+      {
+        buildManifest,
+        remixConfig,
+      },
+      null,
+      2
+    );
+    
+    mkdirSync(".vercel", { recursive: true });
+    writeFileSync(".vercel/remix-build-result.json", `${json}\n`);
+  }
+
   return {
     name: "vercel",
     remixConfig({ remixUserConfig }) {
@@ -49,39 +123,7 @@ export function vercelPreset(): Preset {
          * combined config to determine the server bundle ID.
          */
         serverBundles: remixUserConfig.ssr !== false
-          ? ({ branch }) => {
-              let config = getRouteConfig(branch);
-              if (!config.runtime) {
-                config.runtime = "nodejs";
-              }
-
-              // If there are any "edge" runtime routes, then a special
-              // `entry.server` needs to be used. So copy that file into
-              // the app directory, unless the project has defined their own
-              if (config.runtime === "edge" && !entryServerPath) {
-                let appDirectory = remixUserConfig.appDirectory ?? "app";
-                let entryServerFile = readdirSync(appDirectory).find((f) =>
-                  basename(f, extname(f)) === 'entry.server'
-                );
-                if (entryServerFile) {
-                  entryServerPath = join(appDirectory, entryServerFile);
-                } else {
-                  entryServerPath = join(appDirectory, "entry.server.jsx");
-                  addedEntryServer = true;
-                  cpSync(
-                    join(__dirname, "defaults/entry.server.jsx"),
-                    entryServerPath
-                  );
-                }
-              }
-
-              config = flattenAndSort(config);
-              let id = `${config.runtime}-${hash(config)}`;
-              if (!bundleConfigs.has(id)) {
-                bundleConfigs.set(id, config);
-              }
-              return id;
-            }
+          ? createServerBundles(remixUserConfig)
           : undefined,
 
         /**
@@ -91,40 +133,7 @@ export function vercelPreset(): Preset {
          *     to the `.vercel/remix-build-result.json` file, including
          *     the static configs parsed from each route and server bundle.
          */
-        buildEnd({ buildManifest, remixConfig }) {
-          if (addedEntryServer && entryServerPath) {
-            rmSync(entryServerPath);
-          }
-
-          if (buildManifest?.serverBundles && bundleConfigs.size) {
-            for (let bundle of Object.values(buildManifest.serverBundles)) {
-              let bundleWtihConfig = {
-                ...bundle,
-                config: bundleConfigs.get(bundle.id),
-              };
-              buildManifest.serverBundles[bundle.id] = bundleWtihConfig;
-            }
-          }
-          if (buildManifest?.routes && routeConfigs.size) {
-            for (let route of Object.values(buildManifest.routes)) {
-              let routeWtihConfig = {
-                ...route,
-                config: routeConfigs.get(route.id),
-              };
-              buildManifest.routes[route.id] = routeWtihConfig;
-            }
-          }
-          let json = JSON.stringify(
-            {
-              buildManifest,
-              remixConfig,
-            },
-            null,
-            2
-          );
-          mkdirSync(".vercel", { recursive: true });
-          writeFileSync(".vercel/remix-build-result.json", `${json}\n`);
-        },
+        buildEnd,
       };
     },
   };
